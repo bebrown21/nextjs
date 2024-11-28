03e9d70918df0158012810e2ba29dd00cb0946fd
Brandon Brown <brandonbrownb2@gmail.com>
2024-11-28 16:22:21 -0600

    add modal

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,12 +1,42 @@
 import classes from "./Post.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
+import { useState } from "react";
+import Modal from "./Modal";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Brandon" body="This is awesome" />
-      <Post author="Dude" body="This is lame" />
-    </ul>
+    <>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Dude" body="This is lame" />
+      </ul>
+    </>
   );
 }
 
