2cae92d5cfa7a882c279341aacf78142bae0476a
Brandon Brown <brandonbrownb2@gmail.com>
2024-11-28 17:21:47 -0600

    update modal state

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
 import PostsList from "./components/PostsList";
 
 function App() {
-  return <PostsList />;
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  return (
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />;
+    </>
+  );
 }
 
 export default App;
