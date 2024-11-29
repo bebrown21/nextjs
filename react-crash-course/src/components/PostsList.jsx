85d519c9830cbf4451f7f7a8677cc0391b4e1043
Brandon Brown <brandonbrownb2@gmail.com>
2024-11-29 15:17:03 -0600

    add routing

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,59 +1,29 @@
 import classes from "./Post.module.css";
 import Post from "./Post";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
-function PostsList({ isPosting, onStopPosting }) {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
-
-  function addPostHandler(postData) {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setPosts((prevPosts) => {
-      return [postData, ...prevPosts];
-    });
-  }
+function PostsList() {
+  const data = useLoaderData();
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
-
-      {!isFetching && posts.length > 0 && (
+      {data.posts.length > 0 && (
         <ul className={classes.posts}>
-          {posts.map((post) => (
-            <Post key={post.body} author={post.author} body={post.body} />
+          {data.posts.map((post) => (
+            <Post
+              key={post.id}
+              id={post.id}
+              author={post.author}
+              body={post.body}
+            />
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {data.posts.length === 0 && (
         <div>
           <h2>There are no posts yet</h2>
         </div>
       )}
-      {isFetching && <p>Loading...</p>}
     </>
   );
 }
--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -2,17 +2,35 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
-import NewPost from "./components/NewPost";
+
+import NewPost, { action as newPostAction } from "./routes/NewPost";
+import RootLayout from "./routes/RootLayout";
+import Posts, { loader as postsLoader } from "./routes/Posts";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/create-post",
-    element: <NewPost />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          {
+            path: "/create-post",
+            element: <NewPost />,
+            action: newPostAction,
+          },
+          {
+            path: "/:id",
+            element: <PostDetails />,
+            loader: postDetailsLoader,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
