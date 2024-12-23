6fb034d56613835e3a997a65b89b2a4d906339f5
Brandon Brown <brandonbrownb2@gmail.com>
2024-12-23 11:12:37 -0600

    update file picker component

---
+++ foodies/components/meals/image-picker.js
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+
+  function handlePickImage() {
+    imageInput.current.click();
+  }
+
+  function handleImageChanged(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No Image picked yet</p>}
+          {pickedImage && <Image src={pickedImage} alt="selected image" fill />}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          name={name}
+          accept="image/png, image/jpeg"
+          ref={imageInput}
+          onChange={handleImageChanged}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickImage}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
