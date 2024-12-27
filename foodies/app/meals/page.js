61254916104fe762eade524c89c18eb73f7e299f
Brandon Brown <brandonbrownb2@gmail.com>
2024-12-27 10:05:30 -0600

    add metadata

--- foodies/app/meals/page.js
+++ foodies/app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the meals shared by our community.",
+};
+
 function Meals() {
   const meals = getMeals();
 
