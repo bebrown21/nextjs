ee04a4b9dfe9ee022a5ea34bdf3f5facc446ff2e
Brandon Brown <brandonbrownb2@gmail.com>
2024-12-27 09:39:19 -0600

    add save meal

--- foodies/lib/meals.js
+++ foodies/lib/meals.js
@@ -1,4 +1,7 @@
+import fs from "fs:node";
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xxs from "xss";
 
 const db = sql("meals.db");
 
@@ -8,4 +11,39 @@ export function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export async function saveMeal(meal) {
+  const slug = slugify(meal.title, { lower: true });
+  meal.instructions = xxs(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Failed to save image");
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+
+  db.prepare(
+    `
+      INSERT INTO meals
+       (title, summary, instructions, creator, creator_email, image, slug)
+      VALUES (
+         @title,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email,
+         @image,
+         @slug
+      )
+    `
+  ).run(meal);
 }
